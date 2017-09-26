11a928a122cf613c42ef91f22fd332576bd40249
vovanpap1702 <paprotski1702@gmail.com>
2017-09-26 19:31:22 +0300

    second trial

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,27 @@
 class GuessingGame {
+	var min;
+	var max;
     constructor() {}
 
     setRange(min, max) {
+	    this.min=min;
+	    this.max=max;
+
 
     }
 
     guess() {
 
+	    return Math.round((min+max)/2));
     }
 
     lower() {
+	    max=Math.round((min+max)/2);
 
     }
 
     greater() {
+	    min=Math.round((min+max)/2);
 
     }
 }
